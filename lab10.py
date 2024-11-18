05001464b0adcb4f88e4a644950cdbb260863117
Max Putain <125095778+Satimov727@users.noreply.github.com>
2024-11-18 18:45:21 +0300

    Add files via upload

--- lab10.py
+++ lab10.py
@@ -14,51 +14,64 @@ import math
 # Импортирование модуля tabulate для вывода таблицы.
 from tabulate import tabulate # type: ignore
 
+# Проверка корректности ввода.
+def check_input(input_string):
+    
+    # Проверка на целое число.
+    if input_string.isdigit() or input_string == 0 or input_string.lstrip('-').isdigit():
 
-# Функция для проверки корректности ввода.
-def check_valid_input(user_input):
-    try:
-        float(user_input)
-        return True
-    except ValueError:
-        return False
+        return int(input_string)
+
+    # Проверка на вещественное число.
+    if '.' in input_string:
+        parts = input_string.split('.')
+
+        if len(parts) == 2 and parts[0].lstrip('-').isdigit() and parts[1].isdigit():
+
+            return float(input_string)
+
+    return None
 
 # Получение данных от пользователя.
 a = input("Введите начальную точку интегрирования: ")
-while not check_valid_input(a):
+while check_input(a) is None:
     print("Введено некорректное значение. Начальная точка интегрирования должна быть целым или вещественным числом.")
     a = input("Введите начальную точку интегрирования: ")
 
 b = input("Введите конечную точку интегрирования: ")
-while not check_valid_input(b):
+while check_input(b) is None:
     print("Введено некорректное значение. Конечная точка интегрирования должна быть целым или вещественным числом.")
     b = input("Введите конечную точку интегрирования: ")
 
+while float(b) <= float(a):
+    print("Конечная точка интегрирования должна быть больше начальной точки.")
+    b = input("Введите конечную точку интегрирования: ")
+
 N1 = input("Введите количество участков разбиения N1: ")
-while not check_valid_input(N1):
-    print("Введено некорректное значение. Количество участков разбиения N1 должно быть целым числом.")
+while check_input(N1) is None or float(N1) <= 0 or float(N1) % 1 != 0:
+    print("Введено некорректное значение. Количество участков разбиения N1 должно быть целым положительным числом.")
     N1 = input("Введите количество участков разбиения N1: ")
 
-N2 = int(input("Введите количество участков разбиения N2: "))
-while not check_valid_input(N2):
-    print("Введено некорректное значение. Количество участков разбиения N2 должно быть целым числом.")
+N2 = input("Введите количество участков разбиения N2: ")
+while check_input(N2) is None or float(N2) <= 0 or float(N2) % 1 != 0:
+    print("Введено некорректное значение. Количество участков разбиения N2 должно быть целым положительным числом.")
     N2 = input("Введите количество участков разбиения N2: ")
 
 eps = input("Введите точность ε: ")
-while not check_valid_input(eps):
+while check_input(eps) is None:
     print("Введено некорректное значение. Точность ε должна быть целым или вещественным числом.")
     eps = input("Введите точность ε: ")
 
 a = float(a)
 b = float(b)
 
-# Функция, которую нужно проинтегрировать.
+# Функция, которую нужно проинтегрировать. (Пользователь может менять на произвольную).
 def f(x):
-    return x * math.cos(x)
+    return x * math.sin(x)
 
-# Первообразная функции f(x).
+# Первообразная функции f(x). (Если пользователь меняет функцию на произвольную, то также необходимо поменять и первообразную).
 def F(x):
-    return x * math.sin(x) + math.cos(x)
+    return -x * math.cos(x) + math.sin(x)
 
 # Метод левых прямоугольников.
 def left_rectangle(f, a, b, n):
@@ -147,40 +160,49 @@ data = [
 ]
 
 # Вывод таблицы.
-print(tabulate(data, headers='firstrow', tablefmt='grid'))
+print(tabulate(data, headers='firstrow', tablefmt="simple_grid", missingval='-', colalign=("center", "center", "center")))
 
 # Вывод результата точности.
 print(f"Точность достигнута за {iterations} итераций с N = {N}.")
-print(f"Ошибка: {error:.6f}")
+print(f"Погрешность: {error:.7f}")
 
 # Отображение истинного значения интеграла.
-print(f"\nЗначение интеграла: {true_value:.6f}")
+print(f"\nИстинное значение интеграла: {true_value:.7f}")
 
 # Погрешности для метода левых прямоугольников.
-abs_error_I1 = abs(I1 - true_value)
+abs_error_I1 = abs(I1 - true_value) if I1 is not None else 0
 rel_error_I1 = abs(abs_error_I1 / true_value) if true_value != 0 else 0
-abs_error_I2 = abs(I2 - true_value)
+abs_error_I2 = abs(I2 - true_value) if I2 is not None else 0
 rel_error_I2 = abs(abs_error_I2 / true_value) if true_value != 0 else 0
 
 # Погрешности для метода парабол.
-abs_error_I3 = abs(I3 - true_value)
+abs_error_I3 = abs(I3 - true_value) if I3 is not None else 0
 rel_error_I3 = abs(abs_error_I3 / true_value) if true_value != 0 else 0
-abs_error_I4 = abs(I4 - true_value)
+abs_error_I4 = abs(I4 - true_value) if I4 is not None else 0
 rel_error_I4 = abs(abs_error_I4 / true_value) if true_value != 0 else 0
 
 # Вывод всех погрешностей.
 print("\nПогрешности:")
-print(f"Абсолютная погрешность для метода левых прямоугольников с N1: {abs_error_I1:.6f}")
-print(f"Относительная погрешность для метода левых прямоугольников с N1: {rel_error_I1:.6f}")
-print(f"Абсолютная погрешность для метода левых прямоугольников с N2: {abs_error_I2:.6f}")
-print(f"Относительная погрешность для метода левых прямоугольников с N2: {rel_error_I2:.6f}")
-print(f"Абсолютная погрешность для метода парабол с N1: {abs_error_I3:.6f}")
-print(f"Относительная погрешность для метода парабол с N1: {rel_error_I3:.6f}")
-print(f"Абсолютная погрешность для метода парабол с N2: {abs_error_I4:.6f}")
-print(f"Относительная погрешность для метода парабол с N2: {rel_error_I4:.6f}")
+print(f"Абсолютная погрешность для метода левых прямоугольников с N1: {abs_error_I1:.7f}")
+print(f"Относительная погрешность для метода левых прямоугольников с N1: {rel_error_I1:.7f}")
+print(f"Абсолютная погрешность для метода левых прямоугольников с N2: {abs_error_I2:.7f}")
+print(f"Относительная погрешность для метода левых прямоугольников с N2: {rel_error_I2:.7f}")
+print(f"Абсолютная погрешность для метода парабол с N1: {abs_error_I3:.7f}")
+print(f"Относительная погрешность для метода парабол с N1: {rel_error_I3:.7f}")
+print(f"Абсолютная погрешность для метода парабол с N2: {abs_error_I4:.7f}")
+print(f"Относительная погрешность для метода парабол с N2: {rel_error_I4:.7f}")
 
-# Нахождение наименьшей относительной ошибки среди всех методов.
-min_rel_error = min(rel_error_I1, rel_error_I2, rel_error_I3, rel_error_I4)
+# Нахождение наименьшей относительной погрешности среди всех методов.
+min_rel_error = rel_error_I1
+
+if rel_error_I2 < min_rel_error:
+    min_rel_error = rel_error_I2
+
+if rel_error_I3 < min_rel_error:
+    min_rel_error = rel_error_I3
+
+if rel_error_I4 < min_rel_error:
+    min_rel_error = rel_error_I4
 
 # Определение лучшего метода.
 if min_rel_error == rel_error_I1:
